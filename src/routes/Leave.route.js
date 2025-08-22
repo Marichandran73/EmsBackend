6eb48f263fc6229e313e0aba1f5db2b6f7503869
Marichandran73 <royalmari7339@gmail.com>
2025-08-22 17:31:11 +0530

    feat: i add leave API

--- src/routes/Leave.route.js
+++ src/routes/Leave.route.js
@@ -1,6 +1,6 @@
 import Express from 'express';
 import {
-    AddLeave ,GetLeavesByUserId, GetLeaves ,GetDetails
+    AddLeave ,GetLeavesByUserId, GetLeaves ,GetDetails, UpdateStatus
 } from '../controllers/LeaveController.js';
 import Authmiddleware from '../middleware/authmiddleware.js';
 
@@ -10,6 +10,7 @@ router.post('/addLeave',Authmiddleware, AddLeave);
 router.get('/getLeave/:id',Authmiddleware, GetLeavesByUserId);
 router.get('/AllLeaves',Authmiddleware, GetLeaves);
 router.get('/getDetails/:id',Authmiddleware, GetDetails);
+router.put('/UpdateStatus/:id',Authmiddleware, UpdateStatus);
 
 
 
